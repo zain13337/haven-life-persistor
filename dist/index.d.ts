8a8942834da983554658c01894c40ddc6762205b
Shanks <shanks@havenlife.com>
2019-06-18 11:02:15 -0400

    removing dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export {Persistable, ContainsPersistable, Persistor} from './lib/persistable';
-export {Supertype} from 'supertype';
-export function property(props?: Object);
-export function supertypeClass(target?: any);
