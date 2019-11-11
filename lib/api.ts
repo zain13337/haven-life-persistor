24b5492857ffcdf136fa4939e713c8014d153502
tovahaven <tova@havenlife.com>
2019-11-11 15:43:35 -0500

    returning obectTemplateName even if object isn't persisted

--- lib/api.ts
+++ lib/api.ts
@@ -922,7 +922,8 @@ module.exports = function (PersistObjectTemplate, baseClassForPersist) {
         
         Object.defineProperty(template.prototype, 'objectTemplateName', {
             get: function () {
-                return this._template;
+                //we need to look at the name for cases when the object is not yet persisted and template is not populated yet
+                return this._template || template.__name__;
             },
             enumerable: true,
             configurable: true
